5c092a2105dd292430f38799c0e3a086b6380f26
Bryce Archer <bryce.charles.archer@gmail.com>
2015-02-12 15:26:33 -0800

    routes / strategies complete, profile breaking

--- app/routes.js
+++ app/routes.js
@@ -9,9 +9,11 @@ module.exports = function(app, passport) {
     res.render('login.ejs', { message: req.flash('loginMessage') });
   });
 
-  // app.post('/login', function(req,res) {
-  //
-  // });
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
 
   app.get('/signup', function(req,res) {
     res.render('signup.ejs', { message: req.flash('signupMessage') });
--- config/passport.js
+++ config/passport.js
@@ -41,6 +41,28 @@ module.exports = function(passport) {
         }
       });
     });
-  }
-                                                ));
+  }));
+
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done) {
+    User.findOne({ 'local.email': email }, function(err, user) {
+      if(err){
+        return done(err);
+      }
+
+      if(!user){
+        return done(null, false, req.flash('loginMessage', 'No user found'));
+      }
+
+      if(!user.validPassword(password)){
+        return done(null, false, req.flash('loginMessage', 'Wrong password!'));
+      }
+
+      return done(null, user);
+    });
+  }));
 };
