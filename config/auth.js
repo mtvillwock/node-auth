b7490042292ae76a37137ac66c00d0b5ba10385d
Bryce Archer <bryce.charles.archer@gmail.com>
2015-02-13 15:25:49 -0800

    google auth

--- config/auth.js
+++ config/auth.js
@@ -10,5 +10,10 @@ module.exports = {
     'consumerKey': process.env.TWITTER_CONSUMER_KEY || 'set twitter consumer key',
     'consumerSecret': process.env.TWITTER_CONSUMER_SECRET || 'set twitter consumer secret',
     'callbackURL': 'http://localhost:5000/auth/twitter/callback'
+  },
+  'googleAuth': {
+    'clientID': process.env.GOOGLE_CLIENT_ID || 'set google client id',
+    'clientSecret': process.env.GOOGLE_CLIENT_SECRET || 'set google client secret',
+    'callbackURL': 'http://127.0.0.1:5000/auth/google/callback'
   }
 };
--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,7 @@ var User          = require('../app/models/user');
 var LocalStrategy   = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 //auth variables
 var configAuth = require('./auth');
@@ -137,6 +138,40 @@ module.exports = function(passport) {
             newUser.twitter.token = token;
             newUser.twitter.username = profile.username;
             newUser.twitter.displayName = profile.displayName;
+
+            newUser.save(function() {
+              if(err){
+                throw err;
+              }
+              return done(null, newUser);
+            });
+          }
+        });
+      });
+    }));
+
+    //google strategy
+    passport.use(new GoogleStrategy({
+      clientID : configAuth.googleAuth.clientID,
+      clientSecret : configAuth.googleAuth.clientSecret,
+      callbackURL : configAuth.googleAuth.callbackURL
+    },
+    function(token, refreshToken, profile, done) {
+      process.nextTick(function() {
+        User.findOne({ 'google.id' : profile.id }, function(err, user) {
+          if(err){
+            return done(err);
+          }
+
+          if(user){
+            return done(null, user);
+          } else {
+            var newUser = new User();
+
+            newUser.google.id = profile.id;
+            newUser.google.token = token;
+            newUser.google.name = profile.displayName;
+            newUser.google.email = profile.emails[0].value;
 
             newUser.save(function() {
               if(err){
