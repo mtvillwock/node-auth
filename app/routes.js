b8e5525c54304b155828d0394a42f46796ef2c54
Bryce Archer <bryce.charles.archer@gmail.com>
2015-02-13 18:31:27 -0800

    link ALL THE THINGS!

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,8 @@ module.exports = function(app, passport) {
     res.render('index.ejs');
   });
 
+  //Authentication Routes
+
   app.get('/login', function(req,res) {
     res.render('login.ejs', { message: req.flash('loginMessage') });
   });
@@ -55,11 +57,76 @@ module.exports = function(app, passport) {
             failureRedirect: '/'
           }));
 
+  //Authorization Routes
+  app.get('/connect/local', function(req, res) {
+    res.render('connect-local.ejs', { message: req.flash('loginMessage')});
+  });
+
+  app.post('/connect/local', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/connect/local',
+    failureFlash: true
+  }));
+
+  app.get('/connect/facebook', passport.authorize('facebook', { scope: 'email' }));
+  app.get('/connect/facebook/callback',
+          passport.authorize('facebook', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+          }));
+
+  app.get('/connect/twitter', passport.authorize('twitter', { scope: 'email' }));
+  app.get('/connect/twitter/callback',
+          passport.authorize('twitter', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+          }));
+
+  app.get('/connect/google', passport.authorize('google', { scope: ['profile', 'email'] }));
+  app.get('/connect/google/callback',
+          passport.authorize('google', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+          }));
+
   app.get('/logout', function(req,res) {
     req.logout();
     res.redirect('/');
   });
 
+  //Unlinking Routes
+  app.get('/unlink/local', function(req,res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+  app.get('/unlink/facebook', function(req,res) {
+    var user = req.user;
+    user.facebook.token = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+  app.get('/unlink/twitter', function(req,res) {
+    var user = req.user;
+    user.twitter.token = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+  app.get('/unlink/google', function(req,res) {
+    var user = req.user;
+    user.google.token = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
 };
 
 function isLoggedIn(req, res, next) {
